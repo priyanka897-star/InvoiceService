c3ed683f397d15f807b84cb56e5cb5a1ea620ac3
priyanka897-star <priyankamore8997@gmail.com>
2021-11-11 01:15:00 +0530

    Added:Functionality to Enhance Invoice  calculate Invoice summary using TDD

--- main/java/Ride.java
+++ main/java/Ride.java
@@ -1,6 +1,6 @@
 public class Ride {
-    public final double distance;
-    public final int time;
+    public  final double distance;
+    public  final int time;
 
     public Ride(double distance, int time){
         this.distance = distance ;
--- test/java/InvoiceServiceTest.java
+++ test/java/InvoiceServiceTest.java
@@ -1,33 +1,40 @@
 
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+    @Before
+    public void setUp() throws  Exception{
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+    }
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.CalculateFare(distance,time);
+        double fare = invoiceGenerator.calculateFare(distance,time);
         Assertions.assertEquals(25, fare,0.0);
     }
 
     @Test
     public void givenDistanceAndTime_ShouldReturnMinimumFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.CalculateFare(distance,time);
+        double fare = invoiceGenerator.calculateFare(distance,time);
         Assertions.assertEquals(5, fare,0.0);
     }
 
     @Test
-    public void givenDistanceAndTime_ShouldReturnMultipleRides() {
+    public void givenDistanceAndTime_ShouldReturnInvoiceSummary() {
           InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
           Ride [] rides = {new Ride(2.0, 5),
                             new Ride(0.1, 1)
                             };
-         double fare = invoiceGenerator.CalculateFare(rides);
-        Assertions.assertEquals(30, fare,0.0);
+          InvoiceSummary summary = InvoiceGenerator.calculateFare(rides);
+          InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
+        // double fare = invoiceGenerator.CalculateFare(rides);
+        Assertions.assertEquals(expectedInvoiceSummary,summary);
     }
 }
